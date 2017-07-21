e8fdce1c7a0ac83c817498a47d9260449af06451
yuyi <505063673@qq.com>
2017-07-21 16:29:59 +0800

    完善路由 2017年07月21日16:29:58

--- client/src/index.js
+++ client/src/index.js
@@ -3,19 +3,9 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Link, hashHistory } from 'react-router-dom';
 import style from './public-resource/sass/style.scss';
 
-import Home from './pages/home/home';
-import Create_article from './pages/article/create';
-import Login from './pages/user/login';
-import Register from './pages/user/register';
+import Routes from 'components/routes';
 
 ReactDOM.render(
-  <Router history={hashHistory}>
-    <div>
-      <Route exact path="/" component={Home} />
-      <Route path="/create" component={Create_article} />
-      <Route path="/login" component={Login} />
-      <Route path="/register" component={Register} />
-    </div>
-  </Router>,
+  <Routes />,
   document.getElementById('root')
 );
