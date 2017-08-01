ce3c2069594f6b54856f1d0476cdef842a821770
yuyi <505063673@qq.com>
2017-08-01 16:02:42 +0800

    BrowserRouter 改为 HashRouter 2017年08月01日16:02:40

--- client/src/public-resource/components/routes.js
+++ client/src/public-resource/components/routes.js
@@ -1,5 +1,5 @@
 
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 
 import Home from 'pages/home/home';
 import User from 'pages/user/user';
